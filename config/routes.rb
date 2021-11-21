845ed23e1af80d87d356fcc118213a98a6634f42
Yaroslav09 <bozhakyaroslav@gmail.com>
2021-11-21 15:54:23 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/about', to: 'static_pages#about'
+  get '/advices', to: 'static_pages#advices'
+  get '/contacts', to: 'static_pages#contacts'
+
   root 'static_pages#home'
 end
